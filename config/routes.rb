5c5c51809ee792dbeb0bd4636dd7adc138922a13
sainath biradar <ajay@192-168-1-113.tpgi.com.au>
2021-05-11 18:47:11 +1000

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  root 'emplyoees#index'
+  get '/employees', to: 'employees#show'
+  post '/employees/:id', to: 'employee#create'
+  get '/salaries', to: 'salaries#show'
+  post '/salaries', to: 'salaries#create'
+  get '/shift', to: 'shift#show'
+  post '/shift', to: 'shift#create'
+
+
+
 end
