8a883cc8fd649110da29a3f9e294ecaf241d2a52
sainath biradar <ajay@Ajays-MacBook-Pro.local>
2021-05-12 11:07:48 +1000

    check

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# employees.create(name: 'Sai', email: 'sai@gmail.com', password: 'nill',
